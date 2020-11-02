f1b34590179c804fdd61a2f8b10e3b370dd2e602
Paul <bujor.paul2000@gmail.com>
2020-11-02 20:35:11 +0100

    html cleanup

--- Client/Client/Startup.cs
+++ Client/Client/Startup.cs
@@ -28,7 +28,6 @@ namespace Client
 		{
 			services.AddRazorPages();
 			services.AddServerSideBlazor();
-			services.AddSingleton<WeatherForecastService>();
 			services.AddSingleton<IMap, Map>();
 		}
 
