f059f627d49af020f90da3202fcf7cca5e00009b
Paul <bujor.paul2000@gmail.com>
2020-11-02 18:48:58 +0100

    map showing

---
+++ Client/Client/wwwroot/scripts/LoadMap.js
@@ -0,0 +1,9 @@
+﻿window.mapBoxFunctions = {
+    initMapBox: function () {
+        mapboxgl.accessToken = 'pk.eyJ1IjoiZ2xhZDFvIiwiYSI6ImNraDBzZ2RzMDAxOXcycXJybjBlc2FoYzIifQ.6aVRcqBFu8dA_JC8yVsboA';
+        var map = new mapboxgl.Map({
+            container: 'map',
+            style: 'mapbox://styles/mapbox/streets-v11'
+        });
+    }
+}
