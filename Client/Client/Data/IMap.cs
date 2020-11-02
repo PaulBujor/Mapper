d305291b04629e3dc8fe574508bfbc6e62c1cb27
Paul <bujor.paul2000@gmail.com>
2020-11-02 19:28:22 +0100

    map class

---
+++ Client/Client/Data/IMap.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Client.Data
+{
+	public interface IMap
+	{
+		void SetJSRuntime(IJSRuntime jSRuntime);
+		void InitMapAsync();
+		void AddMarkerAsync(int longitude, int latitude);
+	}
+}
---
+++ Client/Client/Data/Map.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Client.Data
+{
+	public class Map : IMap
+	{
+		[Inject]
+		IJSRuntime JSRuntime { get; set; }
+
+		public void AddMarkerAsync(int longitude, int latitude)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void InitMapAsync()
+		{
+			JSRuntime.InvokeVoidAsync("mapBoxFunctions.initMapBox");
+		}
+
+		public void SetJSRuntime(IJSRuntime jsRuntime)
+		{
+			//JSRuntime = jsRuntime;
+		}
+	}
+}
