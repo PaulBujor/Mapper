b5744e40ed5151f90e3893c9ba56e2590b26ab57
Paul <bujor.paul2000@gmail.com>
2020-11-08 20:31:35 +0100

    rest endpoint for subscribing to new places

--- SpringLogicServer/src/main/java/dk/groupfive/SpringLogicServer/controllers/NetworkController.java
+++ SpringLogicServer/src/main/java/dk/groupfive/SpringLogicServer/controllers/NetworkController.java
@@ -1,8 +1,22 @@
 package dk.groupfive.SpringLogicServer.controllers;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 public class NetworkController {
+    @Autowired
+    public NetworkController() {
 
+    }
+
+    @PutMapping(value = "/subscribe")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public void subscribe(HttpServletRequest request) {
+        System.out.println(request.getRemoteAddr());
+        //todo subscribe to broadcaster
+    }
 }
