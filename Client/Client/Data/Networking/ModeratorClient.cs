63f3db4c4e87623982155c88ac98c6f585aab7d5
Paul <bujor.paul2000@gmail.com>
2020-11-24 17:16:21 +0100

    report menu navigation

---
+++ Client/Client/Data/Networking/ModeratorClient.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Client.Data.Networking
+{
+	public class ModeratorClient
+	{
+		private string URI = "http://127.0.0.1:8080";
+
+		
+	}
+}
