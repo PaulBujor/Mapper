71acc679d42616b03a878201e610a10a8206fe1c
AlexandruTatarciuc <tatarciucalexandru@gmail.com>
2020-11-24 16:05:01 +0100

    review client side

---
+++ Client/Client/Data/ReviewFull.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Client.Data
+{
+    public class ReviewFull : IReview
+    {
+        double rating;
+        List<ReviewItem> reviews;
+
+        public int GetRating()
+        {
+            throw new NotImplementedException();
+        }
+
+        List<ReviewItem> GetReviews()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
