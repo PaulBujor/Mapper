29b4f083f36382e19800e74ae91e1b60b21f27db
Ranteidz <55162517+Ranteidz@users.noreply.github.com>
2020-11-24 14:41:30 +0100

    refactoring

--- SpringLogicServer/src/main/java/dk/groupfive/SpringLogicServer/queue/QueueTest.java
+++ SpringLogicServer/src/main/java/dk/groupfive/SpringLogicServer/queue/QueueTest.java
@@ -1,16 +1,9 @@
 package dk.groupfive.SpringLogicServer.queue;
 
-import com.rabbitmq.client.Channel;
-import com.rabbitmq.client.Connection;
-import com.rabbitmq.client.ConnectionFactory;
-import com.rabbitmq.client.DeliverCallback;
-import dk.groupfive.SpringLogicServer.model.ServerModel;
 import dk.groupfive.SpringLogicServer.model.objects.Place;
-import dk.groupfive.SpringLogicServer.remote.Client;
 
 import java.io.IOException;
 import java.util.List;
-import java.util.concurrent.TimeoutException;
 
 public class QueueTest {
     public static void main(String[] args) throws IOException {
