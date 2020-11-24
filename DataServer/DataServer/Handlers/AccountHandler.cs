cbd2dfa3aacaa97a513b782ccf9d9709be78b762
Paul <bujor.paul2000@gmail.com>
2020-11-24 14:26:23 +0100

    account handler on data tier created

---
+++ DataServer/DataServer/Handlers/AccountHandler.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Sockets;
+using System.Text;
+
+namespace DataServer.Handlers
+{
+	class AccountHandler : IHandler
+	{
+
+        private TcpClient client;
+        private Model model;
+
+        private StreamWriter writer;
+        private StreamReader reader;
+
+        private bool clientConnected;
+
+        public AccountHandler(TcpClient client, Model model)
+        {
+            this.client = client;
+            this.model = model;
+
+            NetworkStream stream = client.GetStream();
+            writer = new StreamWriter(stream, Encoding.ASCII) { AutoFlush = true };
+            reader = new StreamReader(stream, Encoding.ASCII);
+
+        }
+        public void Start()
+		{
+            clientConnected = true;
+            string request = null;
+
+            //todo security protocol for connetion
+
+            // Loop to receive all the data sent by the client.
+            do
+            {
+                try
+                {
+                    request = reader.ReadLine();
+                    Console.WriteLine("Received: {0}", request);
+
+                    ProcessClientRequest(request);
+                }
+                catch (System.IO.IOException e)
+                {
+                    clientConnected = false;
+                }
+
+            } while (clientConnected);
+
+            // Shutdown and end connection
+            client.Close();
+        }
+
+        private void ProcessClientRequest(string request)
+        {
+            switch (request)
+            {
+                case "login":
+                    break;
+                case "register":
+                    break;
+                default:
+                    Console.WriteLine("Default was called");
+                    break;
+            }
+        }
+    }
+}
